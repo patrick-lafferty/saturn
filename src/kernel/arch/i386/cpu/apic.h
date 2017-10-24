5f867b3dad9d7cfd3820102961fdd7064cef1d51
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-10-23 21:28:48 -0400

    added ECX bitchecks for cpuid(1)

---
+++ src/kernel/arch/i386/cpu/apic.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace CPU {
+    void disableLegacyPIC();
+}
--- src/kernel/arch/i386/cpu/cpuid_impl.cpp
+++ src/kernel/arch/i386/cpu/cpuid_impl.cpp
@@ -115,9 +115,49 @@ namespace CPUID {
             printf("BrandString: %s\n", brandString);
         }
 
+        printf("\nFeatures\n--------\n");
+
+        printf("From ECX:\n");
+        
+        printf("SSE3: %d, ", (edx & (1u << 0)) > 0);    
+        printf("PCLMULQDQ: %d, ", (edx & (1u << 1)) > 0);    
+        printf("DTES64: %d, ", (edx & (1u << 2)) > 0);    
+        printf("MONITOR: %d, ", (edx & (1u << 3)) > 0);    
+        printf("DS-CPL: %d, ", (edx & (1u << 4)) > 0);    
+        printf("VMX: %d, ", (edx & (1u << 5)) > 0);    
+        printf("SMX: %d, ", (edx & (1u << 6)) > 0);    
+        printf("EIST: %d", (edx & (1u << 7)) > 0);    
+
+        printf("TM2: %d, ", (edx & (1u << 8)) > 0);    
+        printf("SSSE3: %d, ", (edx & (1u << 9)) > 0);    
+        printf("CNXT-ID: %d, ", (edx & (1u << 10)) > 0);    
+        printf("SDBG: %d, ", (edx & (1u << 11)) > 0);    
+        printf("FMA: %d, ", (edx & (1u << 12)) > 0);    
+        printf("CMPXCHG16B: %d, ", (edx & (1u << 13)) > 0);    
+        printf("xTPR: %d, ", (edx & (1u << 14)) > 0);    
+        printf("PDCM: %d\n", (edx & (1u << 15)) > 0);    
+
+        //bit 16 is reserved 
+        printf("PCID: %d, ", (edx & (1u << 17)) > 0);    
+        printf("DCA: %d, ", (edx & (1u << 18)) > 0);    
+        printf("SSE4.1: %d, ", (edx & (1u << 19)) > 0);
+        printf("SSE4.2: %d, ", (edx & (1u << 20)) > 0);
+        printf("x2APIC: %d, ", (edx & (1u << 21)) > 0);    
+        printf("MOVBE: %d, ", (edx & (1u << 22)) > 0);    
+        printf("POPCNT: %d\n", (edx & (1u << 23)) > 0);   
+
+        printf("TSC-Deadline: %d, ", (edx & (1u << 24)) > 0);  
+        printf("AESNI: %d, ", (edx & (1u << 25)) > 0);    
+        printf("XSAVE: %d, ", (edx & (1u << 26)) > 0);    
+        printf("OSXSAVE: %d, ", (edx & (1u << 27)) > 0);    
+        printf("AVX: %d, ", (edx & (1u << 28)) > 0);    
+        printf("F16C: %d, ", (edx & (1u << 29)) > 0);    
+        printf("RDRAND: %d\n", (edx & (1u << 30)) > 0);    
+        //bit 31 is reserved
+
         /*EDX:
         */
-        printf("\nFeatures\n--------\n");
+        printf("\nFrom EDX:\n");
 
         printf("FPU: %d, ", (edx & (1u << 0)) > 0);    
         printf("VMU: %d, ", (edx & (1u << 1)) > 0);    
