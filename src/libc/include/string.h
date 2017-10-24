8944e002da3cbb508816c4468710e957df3fd743
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-10-24 01:07:25 -0400

    string funcs needed to be external c

--- src/libc/include/string.h
+++ src/libc/include/string.h
@@ -2,6 +2,7 @@
 
 #include <stddef.h>
 
-void* memset(void* destination, int fillByte, size_t count);
-int memcmp(const void* first, const void* second, size_t count);
-void* memmove(void* destination, const void* source, size_t count);
+extern "C" void* memset(void* destination, int fillByte, size_t count);
+extern "C" int memcmp(const void* first, const void* second, size_t count);
+extern "C" void* memmove(void* destination, const void* source, size_t count);
+extern "C" void* memcpy(void* destination, const void* source, size_t count);
