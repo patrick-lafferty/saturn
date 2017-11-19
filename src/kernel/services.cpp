8f969597d1349ec8425609dc17d366563f1f92b9
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-11-19 13:31:26 -0500

    started work on vga service registering

--- src/kernel/services.cpp
+++ src/kernel/services.cpp
@@ -1,12 +1,32 @@
 #include "services.h"
 #include <string.h>
+#include <scheduler.h>
+#include <memory/virtual_memory_manager.h>
 
 namespace Kernel {
+
+    uint32_t RegisterService::MessageId;
+    uint32_t RegisterServiceDenied::MessageId;
+    uint32_t VGAServiceMeta::MessageId;
 
     ServiceRegistry::ServiceRegistry() {
         auto count = static_cast<uint32_t>(ServiceType::ServiceTypeEnd) + 1;
         taskIds = new uint32_t[count];
         memset(taskIds, 0, count * sizeof(uint32_t));
+
+        meta = new ServiceMeta*[count];
+
+        IPC::registerMessage<RegisterService>();
+        IPC::registerMessage<RegisterServiceDenied>();
+        IPC::registerMessage<VGAServiceMeta>();
+    }
+
+    void ServiceRegistry::receiveMessage(IPC::Message* message) {
+        if (message->messageId == RegisterService::MessageId) {
+            auto request = IPC::extractMessage<RegisterService>(
+                    *static_cast<IPC::MaximumMessageBuffer*>(message));
+            registerService(request.senderTaskId, request.type);
+        }
     }
 
     bool ServiceRegistry::registerService(uint32_t taskId, ServiceType type) {
@@ -36,6 +56,35 @@ namespace Kernel {
 
     void ServiceRegistry::setupService(uint32_t taskId, ServiceType type) {
         //TODO: iopb/page map
+
+        switch (type) {
+            case ServiceType::VGA: {
+                auto task = currentScheduler->getTask(taskId);
+
+                if (task == nullptr) {
+                    return;
+                }
+
+                auto vgaPage = task->virtualMemoryManager->allocatePages(1);
+                auto pageFlags = 
+                    static_cast<int>(Memory::PageTableFlags::Present)
+                    | static_cast<int>(Memory::PageTableFlags::AllowWrite)
+                    | static_cast<int>(Memory::PageTableFlags::AllowUserModeAccess);
+                task->virtualMemoryManager->map(vgaPage, 0xB8000, pageFlags);
+
+                auto vgaMeta = new VGAServiceMeta;
+                vgaMeta->vgaAddress = vgaPage;
+                meta[static_cast<uint32_t>(type)] = vgaMeta;
+                vgaMeta->length = sizeof(VGAServiceMeta);
+                vgaMeta->messageId = VGAServiceMeta::MessageId;
+                task->mailbox->send(vgaMeta);
+
+                break;
+            }
+            case ServiceType::ServiceTypeEnd: {
+                break;
+            }
+        }
 
     }
 }
--- src/kernel/services.h
+++ src/kernel/services.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <ipc.h>
 
 namespace Kernel {
 
@@ -9,18 +10,50 @@ namespace Kernel {
         ServiceTypeEnd
     };
 
+    struct RegisterService : IPC::Message {
+        static uint32_t MessageId;
+        ServiceType type;
+    };
+
+    struct RegisterServiceDenied : IPC::Message {
+        static uint32_t MessageId;
+        bool success {false};
+    };
+
+    struct ServiceMeta : IPC::Message {
+    };
+
+    struct VGAServiceMeta : ServiceMeta {
+        static uint32_t MessageId;
+        uint32_t vgaAddress {0};
+    };
+
+    inline uint32_t ServiceRegistryMailbox {0};
+
+    /*
+    A service is a usermode task that controls some frequently used
+    operation, typically requiring special priviledges. There can
+    only be one of each type. When the service task starts,
+    it registers itself with the service which grants the appropriate
+    permissions, and then records its taskId so that other tasks
+    can refer to it by name and not taskId.
+    */
+    inline class ServiceRegistry* ServiceRegistryInstance;
     class ServiceRegistry {
     public:
         
         ServiceRegistry();
         
-        bool registerService(uint32_t taskId, ServiceType type);
-        uint32_t getServiceTaskId(ServiceType type);
+        void receiveMessage(IPC::Message* message);
 
     private:
+        
+        bool registerService(uint32_t taskId, ServiceType type);
+        uint32_t getServiceTaskId(ServiceType type);
 
         void setupService(uint32_t taskId, ServiceType type);
 
         uint32_t* taskIds;
+        ServiceMeta** meta;
     };
 }
