2686ae68b4fae937561659c95e18be70ca549b88
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-11-26 12:25:57 -0500

    started loading a en_US keymap and added a physical key -> uint8_t translation function

--- src/services/keyboard/keyboard.cpp
+++ src/services/keyboard/keyboard.cpp
@@ -1,19 +1,77 @@
 #include "keyboard.h"
 #include <services.h>
 #include <system_calls.h>
+#include <services/terminal/terminal.h>
+#include <string.h>
+#include <stdio.h>
 
 using namespace Kernel;
 
 namespace Keyboard {
 
-    void registerMessages() {
+    uint32_t KeyEvent::MessageId;
 
+    void registerMessages() {
+        IPC::registerMessage<KeyEvent>();
+    }
+
+    uint8_t en_US[128];
+
+    void loadKeymap() {
+        en_US[0x1C] = 'a';
+        en_US[0x32] = 'b';
+        en_US[0x21] = 'c';
+        en_US[0x23] = 'd';
+        en_US[0x24] = 'e';
+        en_US[0x2B] = 'f';
+        en_US[0x34] = 'g';
+        en_US[0x33] = 'h';
+        en_US[0x43] = 'i';
+        en_US[0x3B] = 'j';
+        en_US[0x42] = 'k';
+        en_US[0x4B] = 'l';
+        en_US[0x3A] = 'm';
+        en_US[0x31] = 'n';
+        en_US[0x44] = 'o';
+        en_US[0x4D] = 'p';
+        en_US[0x15] = 'q';
+        en_US[0x2D] = 'r';
+        en_US[0x1B] = 's';
+        en_US[0x2C] = 't';
+        en_US[0x3C] = 'u';
+        en_US[0x2A] = 'v';
+        en_US[0x1D] = 'w';
+        en_US[0x22] = 'x';
+        en_US[0x35] = 'y';
+        en_US[0x1A] = 'z';
+    }
+
+    uint8_t translateKeyEvent(PS2::PhysicalKey key) {
+        /*TODO: keymaps should be stored in files, but since
+        we don't have filesystem support yet, just hardcode
+        en_US in the above array
+        */
+        return en_US[static_cast<uint32_t>(key)];
+    }
+
+    void echo(uint8_t key) {
+        Terminal::PrintMessage message {};
+        message.serviceType = Kernel::ServiceType::Terminal;
+        char s[] = {(char)key, '\0'};
+        memcpy(message.buffer, s, strlen(s));
+        send(IPC::RecipientType::ServiceName, &message);
     }
 
     void messageLoop() {
         while (true) {
             IPC::MaximumMessageBuffer buffer;
             receive(&buffer);
+
+            if (buffer.messageId == KeyEvent::MessageId) {
+                auto event = IPC::extractMessage<KeyEvent>(buffer);
+                auto key = translateKeyEvent(event.key);
+                echo(key);
+            }
         }
     }
 
@@ -31,6 +89,7 @@ namespace Keyboard {
         }
         else if (buffer.messageId == GenericServiceMeta::MessageId) {
             registerMessages();
+            loadKeymap();
             messageLoop();
         }
     }
--- src/services/keyboard/keyboard.h
+++ src/services/keyboard/keyboard.h
@@ -2,10 +2,19 @@
 
 #include <stdint.h>
 #include <ipc.h>
+#include <services/ps2/ps2.h>
 
 namespace Keyboard {
     void service();
 
+    struct KeyEvent : IPC::Message {
+        KeyEvent() {
+            messageId = MessageId;
+            length = sizeof(KeyEvent);
+        }
 
-
+        static uint32_t MessageId;
+        PS2::PhysicalKey key;
+        PS2::KeyStatus status;
+    };
 }
