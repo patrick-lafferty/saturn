29681ffc8aaf80269489842b0145a433428957e5
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-12-02 16:23:07 -0500

    added CPUId object, representing all properties relating to a cpu's identification

--- src/services/hardwareFileSystem/cpu.cpp
+++ src/services/hardwareFileSystem/cpu.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <system_calls.h>
 #include <services/virtualFileSystem/virtualFileSystem.h>
+#include <algorithm>
 
 using namespace VFS;
 using namespace Vostok;
@@ -16,8 +17,167 @@ namespace HardwareFileSystem {
         send(IPC::RecipientType::TaskId, &result);
     }
 
+    /*
+    CPU Id Object
+    */
+
+    CPUIdObject::CPUIdObject() {
+        
+    }
+
+    void CPUIdObject::readSelf(uint32_t requesterTaskId, uint32_t requestId) {
+        ReadResult result;
+        result.success = true;
+        result.requestId = requestId;
+        ArgBuffer args {result.buffer, sizeof(result.buffer)};
+        args.writeType(ArgTypes::Property);
+
+        args.writeValueWithType("model", ArgTypes::Cstring);
+        args.writeValueWithType("familyId", ArgTypes::Cstring);
+        args.writeValueWithType("brand", ArgTypes::Cstring);
+
+        args.writeType(ArgTypes::EndArg);
+
+        result.recipientId = requesterTaskId;
+        send(IPC::RecipientType::TaskId, &result);
+    }
+
+    /*
+    Vostok Object function support
+    */
+    int CPUIdObject::getFunction(std::string_view name) {
+        return -1;
+    }
+
+    void CPUIdObject::readFunction(uint32_t requesterTaskId, uint32_t requestId, uint32_t functionId) {
+        describeFunction(requesterTaskId, requestId, functionId);
+    }
+
+    void CPUIdObject::writeFunction(uint32_t requesterTaskId, uint32_t requestId, uint32_t functionId, ArgBuffer& args) {
+    }
+
+    void CPUIdObject::describeFunction(uint32_t requesterTaskId, uint32_t requestId, uint32_t functionId) {
+    }
+
+    /*
+    Vostok Object property support
+    */
+    int CPUIdObject::getProperty(std::string_view name) {
+        if (name.compare("model") == 0) {
+            return static_cast<int>(PropertyId::Model);
+        }
+        else if (name.compare("familyId") == 0) {
+            return static_cast<int>(PropertyId::FamilyId);
+        }
+        else if (name.compare("brand") == 0) {
+            return static_cast<int>(PropertyId::Brand);
+        }
+
+        return -1;
+    }
+
+    void CPUIdObject::readProperty(uint32_t requesterTaskId, uint32_t requestId, uint32_t propertyId) {
+        ReadResult result;
+        result.success = true;
+        result.requestId = requestId;
+        ArgBuffer args {result.buffer, sizeof(result.buffer)};
+        args.writeType(ArgTypes::Property);
+
+        switch(static_cast<PropertyId>(propertyId)) {
+            case PropertyId::Model: {
+                args.writeValueWithType(model, ArgTypes::Uint32);
+                break;
+            }
+            case PropertyId::FamilyId: {
+                args.writeValueWithType(familyId, ArgTypes::Uint32);
+                break;
+            }
+            case PropertyId::Brand: {
+                args.writeValueWithType(brand, ArgTypes::Cstring);
+                break;
+            }
+        }
+
+        args.writeType(ArgTypes::EndArg);
+
+        result.recipientId = requesterTaskId;
+        send(IPC::RecipientType::TaskId, &result);
+    }
+
+    void CPUIdObject::writeProperty(uint32_t requesterTaskId, uint32_t requestId, uint32_t propertyId, ArgBuffer& args) {
+        auto type = args.readType();
+
+        if (type != ArgTypes::Property) {
+            replyWriteSucceeded(requesterTaskId, requestId, false);
+            return;
+        }
+
+        switch(static_cast<PropertyId>(propertyId)) {
+            case PropertyId::Model: {
+                auto x = args.read<uint32_t>(ArgTypes::Uint32);
+
+                if (!args.hasErrors()) {
+                    model = x;
+                    replyWriteSucceeded(requesterTaskId, requestId, true);
+                    return;
+                }
+            }
+            case PropertyId::FamilyId: {
+                auto x = args.read<uint32_t>(ArgTypes::Uint32);
+
+                if (!args.hasErrors()) {
+                    familyId = x;
+                    replyWriteSucceeded(requesterTaskId, requestId, true);
+                    return;
+                }
+            }
+            case PropertyId::Brand: {
+                auto x = args.read<char*>(ArgTypes::Cstring);
+
+                if (!args.hasErrors()) {
+                    memcpy(brand, x, std::min(static_cast<size_t>(sizeof(brand)), strlen(x)));
+                    replyWriteSucceeded(requesterTaskId, requestId, true);
+                    return;
+                }
+            }
+        }
+
+        replyWriteSucceeded(requesterTaskId, requestId, false);
+    }
+
+    Object* CPUIdObject::getNestedObject(std::string_view name) {
+        return nullptr;
+    }
+
+    /*
+    CPU Id Object specific implementation
+    */
+
+    const char* CPUIdObject::getName() {
+        return "id";
+    }
+
+    /*
+    CPU Object
+    */
+
     CPUObject::CPUObject() {
 
+    }
+
+    void CPUObject::readSelf(uint32_t requesterTaskId, uint32_t requestId) {
+        ReadResult result;
+        result.success = true;
+        result.requestId = requestId;
+        ArgBuffer args {result.buffer, sizeof(result.buffer)};
+        args.writeType(ArgTypes::Property);
+
+        args.writeValueWithType("id", ArgTypes::Cstring);
+
+        args.writeType(ArgTypes::EndArg);
+
+        result.recipientId = requesterTaskId;
+        send(IPC::RecipientType::TaskId, &result);
     }
 
     /*
@@ -41,9 +201,6 @@ namespace HardwareFileSystem {
     Vostok Object property support
     */
     int CPUObject::getProperty(std::string_view name) {
-        if (name.compare("test") == 0) {
-            return static_cast<int>(PropertyId::test);
-        }
 
         return -1;
     }
@@ -56,10 +213,6 @@ namespace HardwareFileSystem {
         args.writeType(ArgTypes::Property);
 
         switch(static_cast<PropertyId>(propertyId)) {
-            case PropertyId::test: {
-                args.writeValueWithType(test, ArgTypes::Cstring);
-                break;
-            }
         }
 
         args.writeType(ArgTypes::EndArg);
@@ -77,18 +230,17 @@ namespace HardwareFileSystem {
         }
 
         switch(static_cast<PropertyId>(propertyId)) {
-            case PropertyId::test: {
-                auto x = args.read<char*>(ArgTypes::Cstring);
-
-                if (!args.hasErrors()) {
-                    memcpy(test, x, sizeof(test));
-                    replyWriteSucceeded(requesterTaskId, requestId, true);
-                    return;
-                }
-            }
         }
 
         replyWriteSucceeded(requesterTaskId, requestId, false);
+    }
+
+    Object* CPUObject::getNestedObject(std::string_view name) {
+        if (name.compare("id") == 0) {
+            return &id;
+        }
+        
+        return nullptr;
     }
 
     /*
@@ -114,12 +266,9 @@ namespace HardwareFileSystem {
         return false;
     }
 
-    void detectCPU() {
-        while (!createCPUObject()) {
-            sleep(10);
-        }
-
-        auto openResult = openSynchronous("/system/hardware/cpu/test");
+    template<typename Arg>
+    void writeTransaction(const char* path, Arg value, ArgTypes argType) {
+        auto openResult = openSynchronous(path);
 
         if (openResult.success) {
             auto readResult = readSynchronous(openResult.fileDescriptor, 0);
@@ -130,14 +279,69 @@ namespace HardwareFileSystem {
                 auto type = args.readType();
 
                 if (type == ArgTypes::Property) {
-                    args.write("cpu detected", ArgTypes::Cstring);
+                    args.write(value, argType);
                 }
 
                 write(openResult.fileDescriptor, readResult.buffer, sizeof(readResult.buffer));
                 receiveAndIgnore();
-                close(openResult.fileDescriptor);
-                receiveAndIgnore();
             }
+
+            close(openResult.fileDescriptor);
+            receiveAndIgnore();
         }
+    }
+
+    void extractCharacters(char* buffer, int index, uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx) {
+        uint32_t masks[] = {0xff, 0xff00, 0xff0000, 0xff000000};
+        uint32_t shifts[] = {0, 8, 16, 24};
+        uint32_t values[] = {eax, ebx, ecx, edx};
+
+        for(int i = 0; i < 16; i++) {
+            buffer[index + i] = (values[i / 4] & masks[i % 4]) >> (shifts[i % 4]);
+        }
+    }
+
+    void getBrandString(char* buffer) {
+        uint32_t eax, ebx, ecx, edx;
+
+        for (auto i = 0; i < 3; i++) {
+            asm("cpuid"
+                : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+                : "a" (0x80000002 + i)
+            );
+
+            extractCharacters(buffer, i * 16, eax, ebx, ecx, edx);
+        }
+    }
+
+    void detectId(uint32_t eax, uint32_t ebx) {
+        auto model = (eax & 0xf0) >> 4;
+        auto familyId = (eax & 0xf00) >> 8;
+        auto brandIndex = (ebx & 0xff);
+
+        writeTransaction("/system/hardware/cpu/id/model", model, ArgTypes::Uint32);
+        writeTransaction("/system/hardware/cpu/id/familyId", familyId, ArgTypes::Uint32);
+        
+        if (!((familyId > 0x0F && model >= 0x03) || brandIndex)) {
+            char brandString[49];
+            brandString[48] = '\0';
+            getBrandString(brandString);
+            writeTransaction("/system/hardware/cpu/id/brand", brandString, ArgTypes::Cstring);
+        }
+    }
+
+    void detectCPU() {
+        while (!createCPUObject()) {
+            sleep(10);
+        }
+
+        uint32_t eax, ebx, ecx, edx;
+
+        asm("cpuid"
+            : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+            : "a" (1)
+        );
+
+        detectId(eax, ebx);
     }
 }
--- src/services/hardwareFileSystem/cpu.h
+++ src/services/hardwareFileSystem/cpu.h
@@ -5,11 +5,47 @@
 
 namespace HardwareFileSystem {
 
+    class CPUIdObject : public HardwareObject {
+    public:
+
+        CPUIdObject();
+        virtual ~CPUIdObject() {}
+
+        void readSelf(uint32_t requesterTaskId, uint32_t requestId) override;
+
+        int getFunction(std::string_view name) override;
+        void readFunction(uint32_t requesterTaskId, uint32_t requestId, uint32_t functionId) override;
+        void writeFunction(uint32_t requesterTaskId, uint32_t requestId, uint32_t functionId, Vostok::ArgBuffer& args) override;
+        void describeFunction(uint32_t requesterTaskId, uint32_t requestId, uint32_t functionId) override;
+
+        int getProperty(std::string_view name) override;
+        void readProperty(uint32_t requesterTaskId, uint32_t requestId, uint32_t propertyId) override;
+        void writeProperty(uint32_t requesterTaskId, uint32_t requestId, uint32_t propertyId, Vostok::ArgBuffer& args) override; 
+
+        Object* getNestedObject(std::string_view name) override;
+
+        const char* getName() override;
+
+    private:
+
+        enum class PropertyId {
+            Model,
+            FamilyId,
+            Brand,    
+        };
+        
+        uint32_t model;
+        uint32_t familyId;
+        char brand[48];
+    };
+
     class CPUObject : public HardwareObject {
     public:
 
         CPUObject();
         virtual ~CPUObject() {}
+
+        void readSelf(uint32_t requesterTaskId, uint32_t requestId) override;
 
         int getFunction(std::string_view name) override;
         void readFunction(uint32_t requesterTaskId, uint32_t requestId, uint32_t functionId) override;
@@ -20,6 +56,8 @@ namespace HardwareFileSystem {
         void readProperty(uint32_t requesterTaskId, uint32_t requestId, uint32_t propertyId) override;
         void writeProperty(uint32_t requesterTaskId, uint32_t requestId, uint32_t propertyId, Vostok::ArgBuffer& args) override;
 
+        Object* getNestedObject(std::string_view name) override;
+
         const char* getName() override;
 
     private:
@@ -29,10 +67,11 @@ namespace HardwareFileSystem {
         };
 
         enum class PropertyId {
-            test
+            Id
         };
 
-        char test[64];
+        CPUIdObject id;
+        
     };
 
     void detectCPU();
