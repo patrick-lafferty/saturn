d54fd99f9b6b12f700b9857b0255e6967fdd5c29
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-12-03 13:08:16 -0500

    refactored the pci prototype

--- src/services/discovery/pci.cpp
+++ src/services/discovery/pci.cpp
@@ -1,5 +1,4 @@
 #include "pci.h"
-#include <stdint.h>
 #include <stdio.h>
 #include <services.h>
 #include <system_calls.h>
@@ -32,127 +31,30 @@ namespace Discovery {
 
         return result;
     }
-
-    struct Register00 {
-        uint16_t vendorId;
-        uint16_t deviceId;
-
-        Register00(uint32_t word) {
-            vendorId = word & 0xFFFF;
-            deviceId = (word >> 16) & 0xFFFF;
-        }
-
-        bool exists() {
-            return vendorId != 0xFFFF && deviceId != 0xFFFF;
-        }
-    };
-
-    struct Register08 {
-        uint8_t revisionId;
-        uint8_t programmingInterface;
-        uint8_t subclass;
-        uint8_t classCode;
-
-        Register08(uint32_t word) {
-            revisionId = word & 0xFF;
-            programmingInterface = (word >> 8) & 0xFF;
-            subclass = (word >> 16) & 0xFF;
-            classCode = (word >> 24) & 0xFF;
-        }
-    };
-
-    struct Register0C {
-        uint8_t cacheLineSize;
-        uint8_t latencyTimer;
-        uint8_t headerType : 7;
-        bool isMultiFunction : 1;
-        uint8_t builtInSelfTest;
-
-        Register0C(uint32_t word) {
-            cacheLineSize = word & 0xFF;
-            latencyTimer = (word >> 8) & 0xFF;
-            headerType = (word >> 16) & 0xFF;
-            builtInSelfTest = (word >> 24) & 0xFF;
-        }
-    };
-
-    namespace PCIBridgeHeader {
-        struct Register18 {
-            uint8_t primaryBusNumber;
-            uint8_t secondaryBusNumber;
-            uint8_t subordinateBusNumber;
-            uint8_t secondaryLatencyTimer;
-
-            Register18(uint32_t word) {
-                primaryBusNumber = word & 0xFF;
-                secondaryBusNumber = (word >> 8) & 0xFF;
-                subordinateBusNumber = (word >> 16) & 0xFF;
-                secondaryLatencyTimer = (word >> 24) & 0xFF;
-            }
-        };
-    }
-
-    enum class ClassCode {
-        Legacy,
-        MassStorageController,
-        NetworkController,
-        DisplayController,
-        MultimediaController,
-        MemoryController,
-        BridgeDevice,
-        SimpleCommunicationsController,
-        BaseSystemPeripherals,
-        InputDevices,
-        DockingStations,
-        Processors,
-        SerialBusControllers,
-        WirelessControllers,
-        IntelligentIOControllers,
-        SatelliteCommunicationsControllers,
-        EncryptionDecryptionControllers,
-        DataAquisitionAndSignalProcessingControllers,
-        Reserved,
-        Undefined = 0xFF
-    };
-
-    enum class BridgeDevice {
-        HostBridge,
-        PCItoPCIBridge,
-    };
-
-    struct HostBridge {
-        uint8_t bus;
-        uint8_t secondaryBus;
-        bool isMultiFunction;
-        bool exists {false};
-    };
-
-    enum class HeaderType {
-        Standard,
-        PCItoPCIBridge,
-        CardBusBridge
-    };
+    
+    using namespace PCI;
+    using namespace PCI::StandardConfiguration;
 
     HostBridge findHostBridge() {
         HostBridge bridge {};
 
         for (uint8_t bus = 0; bus < 255; bus++) {
-            auto device = Register00{readRegister(getAddress(bus, 0, 0, 0))};
+            auto id = Identification{readRegister(getAddress(bus, 0, 0, 0))};
 
-            if (device.exists()) {
-                auto r8 = Register08{readRegister(getAddress(bus, 0, 0, 0x08))};
-                auto rc = Register0C{readRegister(getAddress(bus, 0, 0, 0x0C))};
+            if (id.isValid()) {
+                auto classInfo = Class{readRegister(getAddress(bus, 0, 0, 0x08))};
+                auto header = Header{readRegister(getAddress(bus, 0, 0, 0x0C))};
 
-                if (r8.classCode == static_cast<uint8_t>(ClassCode::BridgeDevice)
-                    && r8.subclass == static_cast<uint8_t>(BridgeDevice::HostBridge)) {
+                if (classInfo.classCode == static_cast<uint8_t>(ClassCode::BridgeDevice)
+                    && classInfo.subclass == static_cast<uint8_t>(BridgeDevice::HostBridge)) {
                     bridge.bus = bus;
                         
-                    if (rc.headerType == static_cast<uint8_t>(HeaderType::PCItoPCIBridge)) {
-                        auto r18 = PCIBridgeHeader::Register18{readRegister(getAddress(bus, 0, 0, 0x18))};
-                        bridge.secondaryBus = r18.secondaryBusNumber;
+                    if (header.headerType == static_cast<uint8_t>(HeaderType::PCItoPCIBridge)) {
+                        auto busInfo = PCIBridgeConfiguration::BusInfo{readRegister(getAddress(bus, 0, 0, 0x18))};
+                        bridge.secondaryBus = busInfo.secondaryBusNumber;
                     }
 
-                    bridge.isMultiFunction = rc.isMultiFunction;
+                    bridge.isMultiFunction = header.isMultiFunction;
                     bridge.exists = true;
 
                     return bridge;
@@ -167,10 +69,10 @@ namespace Discovery {
         const uint8_t devicesPerBridge = 32;
 
         for (uint8_t deviceId = 1; deviceId < devicesPerBridge; deviceId++) {
-            auto device = Register00{readRegister(getAddress(bus, deviceId, 0, 0))};
+            auto id = Identification{readRegister(getAddress(bus, deviceId, 0, 0))};
 
-            if (device.exists()) {
-                printf("vendor: %x device: %x\n", device.vendorId, device.deviceId);
+            if (id.isValid()) {
+                printf("vendor: %x device: %x\n", id.vendorId, id.deviceId);
             }
         }
     }
