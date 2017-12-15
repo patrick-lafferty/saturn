8bf6f8c5c548009d49c8768bb9f37574c9f6e880
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-12-15 12:42:09 -0500

    added run batch file

---
+++ run.bat
@@ -0,0 +1,4 @@
+"c:\Program Files\qemu\qemu-system-i386.exe"^
+    -kernel sysroot/system/boot/saturn.bin^
+    -s -no-reboot -no-shutdown^
+    -hda "C:\Users\pat\VirtualBox VMs\saturn\saturn_test.vdi"
