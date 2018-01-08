dbb5157b6f744aaacba33e159c0c8246fcbc3969
Patrick Lafferty <patrick_lafferty@outlook.com>
2018-01-08 12:47:06 -0500

    first commit of the graphical shell: tests the window manager by floodfilling its window with changing colours

---
+++ src/applications/dsky/main.cpp
@@ -0,0 +1,78 @@
+#include "dsky.h"
+#include <services/windows/messages.h>
+#include <system_calls.h>
+#include <services.h>
+
+using namespace Window;
+
+bool createWindow(uint32_t address) {
+    
+    CreateWindow create;
+    create.serviceType = Kernel::ServiceType::WindowManager;
+    create.bufferAddress = address;
+    
+    send(IPC::RecipientType::ServiceName, &create);
+    
+    IPC::MaximumMessageBuffer buffer;
+    receive(&buffer);
+
+    switch (buffer.messageNamespace) {
+        case IPC::MessageNamespace::WindowManager: {
+            switch (static_cast<MessageId>(buffer.messageId)) {
+                case MessageId::CreateWindowSucceeded: {
+
+                    return true;
+                    break;
+                }
+            }
+
+            break;
+        }
+    }
+
+    return false;
+}
+
+int dsky_main() {
+    auto windowBuffer = new WindowBuffer;
+    auto address = reinterpret_cast<uintptr_t>(windowBuffer);
+
+    if (!createWindow(address)) {
+        delete windowBuffer;
+        return 1;
+    }
+
+    int colourId = 0;
+    uint32_t colours[] = {
+        0xFF'00'00'00, 
+        0x00'FF'00'00, 
+        0x00'00'FF'00, 
+        0x00'00'00'FF, 
+    };
+
+    while (true) {
+
+        Update update;
+        update.serviceType = Kernel::ServiceType::WindowManager;
+        update.x = 0;
+        update.y = 0;
+        update.width = 100;
+        update.height = 100;
+
+        for (int y = 0; y < 100; y++) {
+            for (int x = 0; x < 100; x++) {
+                windowBuffer->buffer[x + y * 800] = colours[colourId]; 
+            }
+        }
+
+        send(IPC::RecipientType::ServiceName, &update);
+
+        colourId++;
+
+        if (colourId == 4) {
+            colourId = 0;
+        }
+
+        sleep(100);
+    }
+}
