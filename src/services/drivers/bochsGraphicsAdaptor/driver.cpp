7849eaec3f950f7320a1c42391aefa22fd3683fa
Patrick Lafferty <patrick_lafferty@outlook.com>
2018-01-08 13:31:06 -0500

    removed the temp freetype test from bga driver

--- src/services/drivers/bochsGraphicsAdaptor/driver.cpp
+++ src/services/drivers/bochsGraphicsAdaptor/driver.cpp
@@ -2,86 +2,13 @@
 #include <services.h>
 #include <system_calls.h>
 #include <services/startup/startup.h>
-#include <ft2build.h>
-#include FT_FREETYPE_H
-#include FT_GLYPH_H
 
 using namespace Kernel;
 
 namespace BGA {
 
-    void registerMessages() {
-
-    }
-
-    struct FreeTypeConfig {
-        FT_Library library;
-        FT_Face face;
-    };
-
-    void messageLoop(uint32_t address, FreeTypeConfig config) {
-        /*auto linearFrameBuffer = reinterpret_cast<uint32_t volatile*>(address);
-
-        auto colour = 0x00'FF'00'00u;
-        auto bufferSize = 800 * 600 * 4;
-
-    
-auto theta = 0.f;
-        while (true) {
-        char* name = "pat";
-        auto index = 0;
-        auto it = name;
-        auto lastPitch = 0;
-        FT_Matrix  matrix;
-        while (*it != '\0') {
-            auto glyphIndex = FT_Get_Char_Index(config.face, *it); 
-            auto error = FT_Load_Glyph(
-                config.face,
-                glyphIndex,
-                0
-            );
-            theta++;
-            auto rad = theta * 3.14159 / 180;
-float sinangle;
-float cosangle;
-asm("fsin" : "=t" (sinangle) : "0" (rad));
-asm("fcos" : "=t" (cosangle) : "0" (rad));
-
-            matrix.xx = cosangle * 3 * 0x10000L ;
-matrix.xy = -sinangle * 0.12 * index * 0x10000L;
-matrix.yx = sinangle + 0;
-matrix.yy = cosangle * 3 * 0x10000L;
-
-
-FT_Glyph glyph;
-FT_Get_Glyph( config.face->glyph, &glyph );
-FT_Glyph_Transform( glyph, &matrix, 0 );
-FT_Vector  origin;
-
-
-origin.x = 32; 
-origin.y = 0;
-            error = FT_Render_Glyph(config.face->glyph, FT_RENDER_MODE_NORMAL);
-            FT_Glyph_To_Bitmap(
-          &glyph,
-          FT_RENDER_MODE_NORMAL,
-          &origin,
-          1 );
-            auto slot = config.face->glyph;
-FT_BitmapGlyph  bit = (FT_BitmapGlyph)glyph;
-            for (int y = 0; y < bit->bitmap.rows; y++) {
-                for (int x = 0; x < bit->bitmap.pitch; x++) {
-                    linearFrameBuffer[x + lastPitch + y * 800] = *bit->bitmap.buffer++;
-                }
-            }
-
-            lastPitch += slot->bitmap.pitch + 100;
-
-            index++;
-            it++;
-        }
-        }
-*/
+    void messageLoop(uint32_t address) {
+        
         Startup::runProgram("/bin/windows.service");
 
         while (true) {
@@ -107,8 +34,6 @@ FT_BitmapGlyph  bit = (FT_BitmapGlyph)glyph;
 
             else if (buffer.messageId == static_cast<uint32_t>(MessageId::VGAServiceMeta)) {
                 auto msg = IPC::extractMessage<VGAServiceMeta>(buffer);
-
-                registerMessages();
 
                 NotifyServiceReady ready;
                 send(IPC::RecipientType::ServiceRegistryMailbox, &ready);
@@ -175,9 +100,7 @@ FT_BitmapGlyph  bit = (FT_BitmapGlyph)glyph;
         printf("[BGA] Id: %d\n", id);
     }
 
-    FreeTypeConfig setupAdaptor() {
-//if (false) {
-        //discover();
+    void setupAdaptor() {
         writeRegister(BGAIndex::Enable, 0);
 
         writeRegister(BGAIndex::XRes, 800);
@@ -185,56 +108,13 @@ FT_BitmapGlyph  bit = (FT_BitmapGlyph)glyph;
         writeRegister(BGAIndex::BPP, 32);
 
         writeRegister(BGAIndex::Enable, 1 | 0x40);
-
-//}
-        FreeTypeConfig config;
-
-        auto error = FT_Init_FreeType(&config.library);
-
-        if (error != 0) {
-            printf("[BGA] Error initializing FreeType: %d\n", error);
-            return {nullptr, nullptr};
-        }
-
-        error = FT_New_Face(config.library,
-            "/system/fonts/OxygenMono-Regular.ttf",
-            0,
-            &config.face);
-
-        if (error != 0) {
-            printf("[BGA] Error creating font face: %d\n", error);
-            return {nullptr, nullptr};
-        }
-
-        error = FT_Set_Char_Size(
-            config.face,
-            0,
-            16 * 64,
-            300,
-            300
-        );
-
-        if (error != 0) {
-            printf("[BGA] Error setting character size: %d\n", error);
-            return {nullptr, nullptr};
-        }
-
-        return config;
     }
 
     void service() {
         auto bgaAddress = registerService();
         sleep(200);
-        auto freetypeConfig = setupAdaptor();
+        setupAdaptor();
 
-        if (freetypeConfig.library == nullptr) {
-            printf("[BGA] Error while setting up freetype, shutting down BGA driver\n");
-            return;
-        }
-        else {
-            printf("[BGA] setup freetype\n");
-        }
-
-        messageLoop(bgaAddress, freetypeConfig);
+        messageLoop(bgaAddress);
     }
 }
