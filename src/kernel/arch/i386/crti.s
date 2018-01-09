6fe569a01eccf12bb727a478f3226b238b392507
Patrick Lafferty <patrick_lafferty@outlook.com>
2018-01-09 12:57:10 -0500

    licensed src/kernel/arch/i386

--- src/kernel/arch/i386/crti.s
+++ src/kernel/arch/i386/crti.s
@@ -1,17 +1,16 @@
 ;/*FROM http://wiki.osdev.org/Calling_Global_Constructors*/
 
-;/* x86 crti.s */
 section .init
 global _init
 _init:
 	push ebp
 	mov ebp, esp
-;	/* gcc will nicely put the contents of crtbegin.o's .init section here. */
+;	
 
 section .fini
 global _fini
 _fini:
 	push ebp
 	mov ebp, esp
-;	/* gcc will nicely put the contents of crtbegin.o's .fini section here. */
+;	
 	
