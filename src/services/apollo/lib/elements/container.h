8584fb0507bb3037634bbcc5aeddafd84305e96c
Patrick Lafferty <patrick_lafferty@outlook.com>
2018-05-24 11:49:59 -0400

    Add documentation for container

--- src/services/apollo/lib/elements/container.h
+++ src/services/apollo/lib/elements/container.h
@@ -35,17 +35,47 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace Apollo::Elements {
 
+	/*
+	Each derived container type may have its own set of meta properties.
+	Those that do get their own namespace defined in here 
+	*/
 	enum class MetaNamespace {
 		Grid		
 	};
 
+	/*
+	MetaData is data that is attached to some UI element that its parent
+	container uses to change how the element is arranged
+
+	For example:
+
+	Grids have rows and columns. Instead of hardcoding a rowNumber variable
+	in the base UIElement, the row number can be supplied from elsewhere.
+	*/
 	struct MetaData {
+		/*
+		ContainerNamespace specifies what container this data belongs to
+		*/
 		MetaNamespace containerNamespace;
+
+		/*
+		Each container can have their own unique set of properties that
+		they expose as attachable metadata, defined in their own enum class.
+		*/
 		uint32_t metaId;
 
+		/*
+		The value of the attached property, currently only int types
+		are supported
+		*/
 		int value;
 	};
 
+	/*
+	Base class for container children. Allows for derived containers
+	to store container-specific data with each child, such as rows
+	and columns for Grid children.
+	*/
 	struct ContainedElement {
 		std::variant<UIElement*, Container*> element;
 		Bounds bounds;
@@ -61,15 +91,44 @@ namespace Apollo::Elements {
 		Container() = default;
 		Container(Configuration& config) : UIElement(config) {}
 
+		/*
+		Adds a single element to the container with default configuration
+		*/
 		virtual void addChild(UIElement* element) = 0;
+
+		/*
+		Adds a single element to the container with some container-specific
+		configuration
+		*/
 		virtual void addChild(UIElement* element, const std::vector<MetaData>& meta) = 0;
+
+		/*
+		Adds a single child container with default configuration
+		*/
 		virtual void addChild(Container* container) = 0;
+
+		/*
+		Adds a single child container with some container-specific
+		configuration
+		*/
 		virtual void addChild(Container* container, const std::vector<MetaData>& meta) = 0;
 
+		/*
+		Arranges the children in some fashion by calculating each
+		child element's bounds
+		*/
 		virtual void layoutChildren() = 0;
 
+		/*
+		Returns the bounds of the given child. Bounds are calculated
+		in a layoutChildren call and stored in the ContainedElement
+		*/
 		virtual Bounds getChildBounds(const UIElement* child) = 0;
 
+		/*
+		Forwards text layout requests up the ui hierarchy until it
+		reaches the Window, which is capable of performing the request.
+		*/
         virtual void requestLayoutText(UIElement* element) {
 			auto parent = getParent();
 
@@ -78,6 +137,10 @@ namespace Apollo::Elements {
 			}
 		}
 
+		/*
+		Forwards render requests up the ui hierarchy until it
+		reaches the Window, which is capable of performing the request.
+		*/
         virtual void requestRender(UIElement* element) {
 			auto parent = getParent();
 
