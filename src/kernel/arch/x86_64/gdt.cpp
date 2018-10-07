dfd581a5c8906e18794f38200307ae70c6419f57
Patrick Lafferty <patrick_lafferty@outlook.com>
2018-10-07 15:58:16 -0400

    Add 64-bit GDT

---
+++ src/kernel/arch/x86_64/gdt.cpp
@@ -0,0 +1,63 @@
+/*
+Copyright (c) 2018, Patrick Lafferty
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holder nor the names of its 
+      contributors may be used to endorse or promote products derived from 
+      this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include "gdt.h"
+
+uint64_t Gdt[3];
+GDT::DescriptorPointer<uint64_t> GdtPointer;
+
+extern "C" void gdt_flush();
+
+namespace GDT {
+
+    template<typename... Arg> uint32_t combineFlags(Arg... args) {
+        return (static_cast<uint32_t>(args) | ...);
+    }
+
+    int nextGDTIndex = 0;
+
+    void setup() {
+
+        GdtPointer.limit = 8 * 3 - 1;
+        GdtPointer.base = reinterpret_cast<uint64_t>(&Gdt[0]);
+
+        /* 
+        These values are based off of the tables in AMD's
+        "AMD64 Architecture Programmer's Manual Volume 2",
+        pages 88-89
+        */
+        Gdt[0] = 0;
+        Gdt[1] = (0b00000000'00100000'10011000'00000000ull) << 32;
+        //Even though AMD says W bit is ignored, Intel requires it
+        Gdt[2] = (0b00000000'00000000'10010010'00000000ull) << 32;
+
+        nextGDTIndex = 3;
+
+        gdt_flush();
+    }
+
+}
